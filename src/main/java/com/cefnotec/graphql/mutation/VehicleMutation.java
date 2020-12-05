6bdbce8ceca7eb1c1ee345e415592c2469fc6209
trickered <brrumur476@gmail.com>
2020-12-05 15:47:14 -0600

    se sube el lab

---
+++ src/main/java/com/cefnotec/graphql/mutation/VehicleMutation.java
@@ -0,0 +1,19 @@
+package com.cefnotec.graphql.mutation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cefnotec.graphql.entities.Vehicle;
+import com.cefnotec.graphql.services.VehicleService;
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+	@Autowired
+	private VehicleService vehicleService;
+
+	public Vehicle createVehicle(String type, String modelCode, String brandName, String launchDate) {
+		return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+
+	}
+}
---
+++ src/main/java/com/cefnotec/graphql/query/VehicleQuery.java
@@ -0,0 +1,25 @@
+package com.cefnotec.graphql.query;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.cefnotec.graphql.entities.Vehicle;
+import com.cefnotec.graphql.services.VehicleService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+@Component
+public class VehicleQuery implements GraphQLQueryResolver {
+	@Autowired
+	private VehicleService vehicleService;
+
+	public List<Vehicle> getVehicles(int count) {
+		return this.vehicleService.getAllVehicles(count);
+	}
+
+	public Optional<Vehicle> getVehicle(int id) {
+		return this.vehicleService.getVehicle(id);
+	}
+}
